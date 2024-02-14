2b3593eabdb622c1fdbb7a583edee59db76a84f7
ValdrinH <95773922+ValdrinH@users.noreply.github.com>
2024-02-14 23:42:05 +0100

    Fixed

---
+++ AppointEase/AppointEase.Application.Contracts/Identity/ApplicationRoles.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointEase.Application.Contracts.Identity
+{
+    [Table("Roles")]
+    public class ApplicationRoles : IdentityRole
+    {
+        public string Pershkrimi { get; set; }
+    }
+}
---
+++ AppointEase/AppointEase.Data/DataInjectionServices.cs
@@ -0,0 +1,39 @@
+﻿using AppointEase.Application.Contracts.Identity;
+using AppointEase.Application.Contracts.Interfaces;
+using AppointEase.Application.Contracts.Models;
+using AppointEase.Data.Data;
+using AppointEase.Data.Models;
+using AppointEase.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointEase.Data
+{
+    public static class DataInjectionServices
+    {
+        public static void AddDataServices(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors.AddDbContext<AppointEaseContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")
+                );
+            });
+            
+
+            serviceDescriptors.AddScoped<AppointEaseContext>();
+            serviceDescriptors.AddScoped<IRepository<TblPacient>, UserRepository>();
+            serviceDescriptors.AddScoped<IRepository<TblAdmin>, AdminRepository>();
+            serviceDescriptors.AddScoped<IRepository<TblClinic>, ClinicRepository>();
+            serviceDescriptors.AddScoped<IRepository<TblDoctor>, DoctorReporsitory>();
+
+        }
+    }
+}
