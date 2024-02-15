0cfa025d4288e3217de257ddc39105571f642b35
ValdrinH <95773922+ValdrinH@users.noreply.github.com>
2024-02-15 18:09:21 +0100

    Fixed

---
+++ AppointEase/AppointEase.Application/Services/AuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointEase.Application.Services
+{
+    public class AuthenticationService
+    {
+    }
+}
